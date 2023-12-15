d4ddeea241dd2481cb9b241194124c64d7f2081f
irobertcostin <irobertcostin@gmail.com>
2023-12-15 08:17:38 +0100

    created controller and DTOs

---
+++ src/book/dto/update-book.dto.ts
@@ -0,0 +1,12 @@
+import { Category } from "../schemas/book.schema"
+
+
+
+
+export class UpdateBookDto {
+    readonly title: string
+    readonly author: string
+    readonly description: string
+    readonly price: number
+    readonly category: Category
+}
