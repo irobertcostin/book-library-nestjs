e2b8a6538c986549d34f9b2033dd724be4bd3514
irobertcostin <irobertcostin@gmail.com>
2023-12-16 12:56:21 +0100

    jwt implementation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,31 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/users.schema';
 import { UsersController } from './users.controller';
+import { JwtModule } from "@nestjs/jwt"
+import { PassportModule } from "@nestjs/passport"
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+
+      inject: [ConfigService],
+
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_VALIDITY')
+          }
+        }
+      }
+    })
+  ],
 
 })
 export class UsersModule { }
