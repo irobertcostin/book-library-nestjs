16116127be06de03307eb9b3186b0df01162b645
irobertcostin <irobertcostin@gmail.com>
2023-12-16 12:55:25 +0100

    refined user schema

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { IsStrongPasswordOptions } from "class-validator";
 
 
 export enum MaritalStatus {
@@ -21,6 +22,15 @@ export class User {
 
     @Prop()
     last_name: string;
+
+    @Prop({ unique: [true, 'This username is already in use'] })
+    username: string
+
+    @Prop({ unique: [true, 'This email is already registered'] })
+    email: string
+
+    @Prop()
+    password: string
 
     @Prop()
     age: number;
